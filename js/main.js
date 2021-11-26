2f2afdb53d8439f2038b2d4d91e5026e86f35b1e
fuchsia-84 <mikitech9084@gmail.com>
2021-11-26 19:09:46 +0900

    fix global navigation style and add animations.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(function() {
+    $toggle_btn = $('.toggle_btn');
+    $global_nav = $('#global_nav');
+
+    /* toggle button clicked, show global navigation*/
+    $toggle_btn.click(function() {
+        $global_nav.slideToggle();
+        $toggle_btn.toggleClass('show');
+    });
+
+    /* style gallery for PC */
     $('#gallery_pc').flickity({
         autoPlay: true,
         draggble: false
