e24158d2b418855313faa66616b2a9c27d5f39b8
fuchsia-84 <mikitech9084@gmail.com>
2021-11-26 20:30:29 +0900

    fix header style: window scrolled.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 $(function() {
     $toggle_btn = $('.toggle_btn');
     $global_nav = $('#global_nav');
+    $header = $('#header');
+    window_height = $(window).height();
+
 
     /* toggle button clicked, show global navigation*/
     $toggle_btn.click(function() {
         $global_nav.slideToggle();
         $toggle_btn.toggleClass('show');
+    });
+
+    /* scroll over view height, change header style */
+    $(window).on('scroll', function() {
+        if (window_height < $(window).scrollTop()) {
+            $header.addClass('fixed');
+        } else {
+            $header.removeClass('fixed');
+        }
     });
 
     /* style gallery for PC */
